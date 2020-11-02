653da6cef1941ac4e56ec8abb00e780bb0130c19
Al Brancozzi <al.brancozzi@gmail.com>
2020-11-02 22:28:03 +0100

    add Home screen

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
 import React from "react";
 import { StyleSheet, useWindowDimensions, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
 import Draw from "./feature/Draw";
 import Calendar from "./feature/Calendar";
+import Home from "./feature/Home";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   const { width: winWidth, height: winHeight } = useWindowDimensions();
   return (
-    <View
-      style={{
-        ...styles.container,
-        marginTop: winHeight * 0.04,
-      }}
-    >
-      {/* <Draw winWidth={winWidth} winHeight={winHeight} /> */}
-      <Calendar winWidth={winWidth} />
-    </View>
+    // <View
+    //   style={{
+    //     ...styles.container,
+    //     marginTop: winHeight * 0.04,
+    //   }}
+    // >
+    //   {/* <Draw winWidth={winWidth} winHeight={winHeight} /> */}
+    //   {/* <Calendar winWidth={winWidth} /> */}
+    //   <Home />
+    // </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home" headerMode="none">
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ feature/Home.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+
+const Home = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.buttonWrapper}>
+        <Pressable style={styles.button}>
+          <Text>Record for today</Text>
+        </Pressable>
+        <Pressable style={styles.button}>
+          <Text>Calendar</Text>
+        </Pressable>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1,
+  },
+  buttonWrapper: {
+    justifyContent: "space-around",
+    alignItems: "center",
+    width: "100%",
+    height: "25%",
+  },
+  button: {
+    justifyContent: "space-around",
+    alignItems: "center",
+    width: "90%",
+    height: "40%",
+    borderRadius: 20,
+    backgroundColor: "pink",
+  },
+});
+
+export default Home;
